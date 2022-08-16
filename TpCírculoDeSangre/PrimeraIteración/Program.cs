c1c5698b0d44838f71909c0d6a8e78ba64963038
luchooovilla <lucianovilla00@gmail.com>
2022-08-16 16:01:05 -0300

    Revert "Cambio"
    This reverts commit 1d322ce38b427eb0da8e9dc449dee5922344948f.

---
+++ TpCírculoDeSangre/PrimeraIteración/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socio socio = new Socio();
+            socio.ingresardatos();
+            var validation = new ValidacionSocio();
+            var resultado = validation.Validate(socio); //guarda resultados de validacion
+            if(resultado.IsValid)
+            {
+                socio.mostrardatos();
+            }
+            else
+            {
+                foreach (var error in resultado.Errors)
+                {
+                    Console.WriteLine(error.PropertyName + ": " + error.ErrorMessage);
+                }
+            }
+        }
+    }
+}
