d371be6d2c7e2a116b64f2456771f9f81e1ef0da
luchooovilla <104120216+luchooovilla@users.noreply.github.com>
2022-11-22 20:29:38 -0300

    Update PedidoDonacion.cs

--- TpCírculoDeSangre/Código/CirculoSangre/TP - Circulo de Sangre/Clases/PedidoDonacion.cs
+++ TpCírculoDeSangre/Código/CirculoSangre/TP - Circulo de Sangre/Clases/PedidoDonacion.cs
@@ -4,30 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TP___Circulo_de_Sangre.Clases
+namespace TP___Circulo_de_Sangre
 {
     public class PedidoDonacion
     {
         public string grupoSangre;
+        
         public int cantSolicitadaDonantes;
+        
         public DateTime fechaDeSolicitud;
 
-        public string GrupoSangre { get => grupoSangre; set => grupoSangre = value; }
-        public int CantSolicitadaDonantes { get => cantSolicitadaDonantes; set => cantSolicitadaDonantes = value; }
-        public DateTime FechaDeSolicitud { get => fechaDeSolicitud; set => fechaDeSolicitud = value; }
+        private string GrupoSangre 
+        { 
+            get => grupoSangre; 
+            set => grupoSangre = value; 
+        }
+        private int CantSolicitadaDonantes 
+        { 
+            get => cantSolicitadaDonantes; 
+            set => cantSolicitadaDonantes = value; 
+        }
+        private DateTime FechaDeSolicitud 
+        { 
+            get => fechaDeSolicitud; 
+            set => fechaDeSolicitud = value; 
+        }
 
 
         public void cargarSolicitudDonantes()
         {
             Console.WriteLine("Por favor ingrese la fecha para la que solicita donantes");
+            
             fechaDeSolicitud = Convert.ToDateTime(Console.ReadLine());
+            
             Console.WriteLine("Por favor ingrese el grupo de Sangre que desea solicitar");
+            
             grupoSangre = Console.ReadLine();
+            
             Console.WriteLine("Por favor ingrese la cantidad de donantes que desea solicitar");
+            
             cantSolicitadaDonantes = Convert.ToInt32(Console.ReadLine());
 
             notificarDonantes();
-            //asignarDonantes();
         }
 
         public void notificarDonantes()
